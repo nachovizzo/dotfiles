9a4edbb0ce12dea86b8d0d306571063318b7fa6e
Ignacio Vizzo <ignaciovizzo@gmail.com>
2023-04-13 16:50:16 +0200

    FInish config

--- .config/nvim/plugin/lua/bufferline.lua
+++ .config/nvim/plugin/lua/bufferline.lua
@@ -1,24 +1,24 @@
 -- Buffer line setup
-require "bufferline".setup {
-    options = {
-        indicator = {style = "icon", icon = " "},
-        buffer_close_icon = "",
-        modified_icon = "●",
-        close_icon = "",
-        close_command = "bdelete %d",
-        right_mouse_command = "bdelete! %d",
-        left_trunc_marker = "",
-        right_trunc_marker = "",
-        offsets = {{filetype = "NvimTree", text = "EXPLORER", text_align = "center"}},
-        show_tab_indicators = true,
-        show_close_icon = false
-    },
-    highlights = {
-        fill = {
-            bg = {
-                attribute = "bg",
-                highlight = "Pmenu"
-            }
-        }
-    }
-}
+require("bufferline").setup({
+	options = {
+		indicator = { style = "icon", icon = " " },
+		buffer_close_icon = "",
+		modified_icon = "●",
+		close_icon = "",
+		close_command = "bdelete %d",
+		right_mouse_command = "bdelete! %d",
+		left_trunc_marker = "",
+		right_trunc_marker = "",
+		offsets = { { filetype = "NvimTree", text = "EXPLORER", text_align = "center" } },
+		show_tab_indicators = true,
+		show_close_icon = false,
+	},
+	highlights = {
+		fill = {
+			bg = {
+				attribute = "bg",
+				highlight = "Pmenu",
+			},
+		},
+	},
+})
--- .config/nvim/plugin/lua/telescope.lua
+++ .config/nvim/plugin/lua/telescope.lua
@@ -1,25 +1,30 @@
 local actions = require("telescope.actions")
-require("telescope").setup{
-  defaults = {
-    mappings = {
-      i = {
-        ["<esc>"] = actions.close
-      },
-    },
-  },
-  pickers = {
-    grep_string = {
+require("telescope").setup({
+	defaults = {
+		mappings = {
+			i = {
+				["<esc>"] = actions.close,
+			},
+		},
+	},
+	pickers = {
+		live_grep = {
 			prompt_prefix = "🔍 ",
-      initial_mode = "normal",
-    },
-    find_files = {
-      theme = "dropdown",
-      prompt_title = '';
-      previewer = false,
-      prompt_prefix = 'Files> ';
-      layout_config = {
-        width = 0.8,
-      },
-    }
-  },
-}
+		},
+		grep_string = {
+			initial_mode = "normal",
+			results_title = false,
+			prompt_title = false,
+			previewer_title = false,
+		},
+		find_files = {
+			theme = "dropdown",
+			prompt_title = "",
+			previewer = false,
+			prompt_prefix = "Files> ",
+			layout_config = {
+				width = 0.8,
+			},
+		},
+	},
+})
