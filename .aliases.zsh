079c287c81da531b5a8a12e9863776d2abd1cfc0
Ignacio Vizzo <ignaciovizzo@gmail.com>
2023-08-21 10:56:19 +0200

    Forger research aliases

--- .aliases.zsh
+++ .aliases.zsh
@@ -3,33 +3,22 @@
 #
 # Copyright (c) 2022 Ignacio Vizzo, all rights reserved
 alias -g latest='*(om[1])'
-alias py='ipython3'
-alias coit='code . && exit'
-alias cor='code -r'
-
+alias cb="cmake --build build -j"
+alias cm="cmake -Bbuild -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
+alias de='cd ~/dev'
 alias deko='cd ~/Desktop'
 alias dekoo='cd ~/Desktop'
-alias ex='cd ~/dev/examples'
-alias te='cd ~/Teaching'
-alias de='cd ~/dev'
-alias re='cd ~/Research/'
-alias cv='cd ~/.config/nvim/'
-
-alias xopen='xdg-open'
-alias open='xdg-open'
-alias xcopy='xclip -selection clipboard'
-alias ll='ls -hltrBF --color=auto'
 alias df='df -h -xsquashfs -xtmpfs -xdevtmpfs'
+alias ex='cd ~/dev/examples'
+alias gs="git status"
+alias ll='ls -hltrBF --color=auto'
+alias meld='light_meld'
+alias mkae='make'
+alias py='ipython3'
 alias tkb='take build'
-alias vim='nvim'
 alias tmux='tmux -2'
 alias ts='tmux new-session -s $(basename $(pwd))'
+alias vim='nvim'
+alias xcopy='xclip -selection clipboard'
+alias xopen='xdg-open'
 alias ydam='yadm'
-alias mkae='make'
-alias cat='bat'
-alias meld='light_meld'
-alias gs="git status"
-
-alias cm="cmake -Bbuild -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
-alias cb="cmake --build build -j"
-alias got="git"
