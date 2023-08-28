fe8f6eb74bb77f73fb0c65c02124ffb076c1000c
Ignacio Vizzo <ignaciovizzo@gmail.com>
2023-08-28 13:33:20 +0200

    ros2

--- .functions.zsh
+++ .functions.zsh
@@ -1,3 +1,8 @@
+ros2_setup() {
+  source /opt/ros/humble/setup.zsh
+  complete -o nospace -o default -F _python_argcomplete "ros2"
+}
+
 get_remote() {
     git remote -vv | tail -n1 | awk '{print $2}' | tr -d '\n' | xcopy
 }
