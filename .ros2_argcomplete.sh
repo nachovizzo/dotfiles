32391a6d8cfae09a20e9944fb8869d2bae03be14
Ignacio Vizzo <ignacio@dexory.com>
2024-02-01 15:26:51 +0100

    Check on this

--- .ros2_argcomplete.sh
+++ .ros2_argcomplete.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env zsh
 # Fix zsh autocomplete in zsh
 # https://github.com/ros2/ros2cli/issues/534#issuecomment-957516107
-eval "$(register-python-argcomplete3 ros2)"
-eval "$(register-python-argcomplete3 colcon)"
+
+# Fix zsh autocomplete in zsh
+if [ -f /opt/ros/${ROS_DISTRO}/setup.zsh ]; then
+  source /opt/ros/${ROS_DISTRO}/setup.zsh
+  complete -o nospace -o default -F _python_argcomplete "ros2"
+  eval "$(register-python-argcomplete3 ros2)"
+  eval "$(register-python-argcomplete3 colcon)"
+fi
